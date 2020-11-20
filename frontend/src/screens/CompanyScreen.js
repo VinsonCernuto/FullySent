86aed707820a67919012d08f67cf2428d5a37cdd
LyricDuda <Lyricsduda@gmail.com>
2020-11-20 13:55:18 -0500

    Fixed some stuff with the homepage

--- frontend/src/screens/CompanyScreen.js
+++ frontend/src/screens/CompanyScreen.js
@@ -3,7 +3,70 @@ import React from 'react'
 export default function CompanyScreen() {
     return (
         <div>
-            CompanyScreen
+           <div class="main">
+			<div class="shop_top">
+				<div class="container">
+					<h3 class="m_2">Our Team</h3>
+					<div class="row team_box">
+						<div class="col-md-3 team1">
+							<a class="popup-with-zoom-anim" href="#small-dialog3"><img src="assets\img\heros\_DSC7743.jpg" class="img-responsive" title="continue" alt="" /></a>
+							<div id="small-dialog3" class="mfp-hide">
+								<div class="pop_up2">
+									<h2>Lorem ipsum </h2>
+									<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie.</p>
+									<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie.</p>
+								</div>
+							</div>
+							<h4 class="m_5"><a href="#">Lorem Ipsum is simply dummy</a></h4>
+							<p class="m_6">sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna</p>
+						</div>
+						<div class="col-md-3 team1">
+							<a class="popup-with-zoom-anim" href="#small-dialog3"><img src="assets\img\heros\Compound 5-9-20-44.jpg" class="img-responsive" title="continue" alt="" /></a>
+							<div id="small-dialog3" class="mfp-hide">
+								<div class="pop_up2">
+									<h2>Lorem ipsum </h2>
+									<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie.</p>
+									<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie.</p>
+								</div>
+							</div>
+							<h4 class="m_5"><a href="#">Lorem Ipsum is simply dummy</a></h4>
+							<p class="m_6">sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna</p>
+						</div>
+						<div class="col-md-3 team1">
+							<a class="popup-with-zoom-anim" href="#small-dialog3"><img src="assets\img\heros\C0344.00_03_20_58.Still004.jpg" class="img-responsive" title="continue" alt="" /></a>
+							<div id="small-dialog3" class="mfp-hide">
+								<div class="pop_up2">
+									<h2>Lorem ipsum </h2>
+									<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie.</p>
+									<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie.</p>
+								</div>
+							</div>
+							<h4 class="m_5"><a href="#">Lorem Ipsum is simply dummy</a></h4>
+							<p class="m_6">sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna</p>
+						</div>
+						<div class="col-md-3 team1">
+							<a class="popup-with-zoom-anim" href="#small-dialog3"><img src="assets\img\heros\three wheel 3.jpg" class="img-responsive" title="continue" alt="" /></a>
+							<div id="small-dialog3" class="mfp-hide">
+								<div class="pop_up2">
+									<h2>Lorem ipsum </h2>
+									<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie.</p>
+									<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie.</p>
+								</div>
+							</div>
+							<h4 class="m_5"><a href="#">Lorem Ipsum is simply dummy</a></h4>
+							<p class="m_6">sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna</p>
+						</div>
+					</div>
+					<div class="col-md-8">
+						<ul class="team_list">
+							<h4>Vision Statement</h4>
+							<p class="m_7">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+							<img src="assets\img\heros\C0306.00_00_58_07.Still001.jpg" class="img-responsive" alt="" />
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
         </div>
     )
 }
