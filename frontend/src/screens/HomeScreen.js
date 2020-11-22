7d812ea6ed28b742f30d322daf2f84113f74c90c
Vinson Cernuto <cernutovinson@gmail.com>
2020-11-22 17:09:40 -0500

    homepage carousel and product slider complete

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,44 +1,128 @@
 import React from 'react'
+import Carousel from 'react-bootstrap/Carousel'
+import Button from 'react-bootstrap/Button'
+import InfiniteCarousel from 'react-leaf-carousel';
+
+
 import zHero from "../images/heros/Corey350Z.jpg";
 import clutchkickers from "../images/heros/clutchkickers.gif";
 
+import hoodie from '../images/fullysentProducts/Season1/FullySentBoxLogoHoddie/FullySentBoxLogoHoddie-Black.jpg';
+import hat from '../images/fullysentProducts/Season1/FullySent5Panel/FullySent5Panel-black.jpg';
+import tee from '../images/fullysentProducts/Season1/FullySentBoxLogoShirt/FullySentBoxLogoShirt-Black.jpg';
+import yetti from '../images/fullysentProducts/Season1/FullySentTumbler/FullySentTumbler-Black.png';
+import boxTee from '../images/fullysentProducts/Season1/FullySentBoxLogoShirt/FullySentBoxLogoShirt-Black.jpg';
+import grimHoodie from '../images/fullysentProducts/Season3[SpringBreakDro2019]/grimHoodie/grimHoodieFront.jpg'
 
 export default function HomeScreen() {
 	return (
 		<div>
-			<div classname="banner">
-				<div id="fwslider">
-					<div classname="slider_container">
-						<div classname="slide">
+			<Carousel>
+				<Carousel.Item interval={2500}>
+					<img
+						className="d-block w-100"
+						src={zHero}
+						alt="First slide"
+					/>
+					<Carousel.Caption>
+						<h3>Shop The New Collections!</h3>
+						<p><Button variant="primary" src='./screens/ProductScreen'>Shop Now</Button></p>
+					</Carousel.Caption>
+				</Carousel.Item>
+				<Carousel.Item interval={9500}>
+					<img
+						className="d-block w-100"
+						src={clutchkickers}
+						alt="Third slide"
+					/>
+					<Carousel.Caption>
+						<h3>Shop The New Collections!</h3>
+						<p><Button variant="primary" src='./screens/ProductScreen'>Shop Now</Button></p>
+					</Carousel.Caption>
+				</Carousel.Item>
+			</Carousel>
+			<br></br>
+			<br></br>
+			<br></br>
+			<br></br>
+			<br></br>
+			<br></br>
 
-							<img src={zHero} class="img-responsive" alt="" />
-
-							<div classname="slide_content">
-								<div classname="slide_content_wrap">
-
-									<h1 class="title"><br />
-								Established 2017<br />
-								Melbourne, Florida</h1>
-									<div classname="button"><a href="#">See Details</a></div>
-								</div>
-							</div>
-						</div>
-						<div classname="slide">
-							<img src={clutchkickers} class="img-responsive" alt="" />
-							<div classname="slide_content">
-								<div classname="slide_content_wrap">
-									<h1 class="title">Shop New<br></br>Cloections</h1>
-									<div classname="button"><a href="#">See Details</a></div>
-								</div>
-							</div>
-						</div>
+			<div className="product-slider">
+				<h2>Best Sellers</h2>
+				<br></br>
+				<p>Shop some of our top selling items</p>
+				<br></br>
+				<br></br>
+				<br></br>
+				<InfiniteCarousel
+					breakpoints={[
+						{
+							breakpoint: 500,
+							settings: {
+								slidesToShow: 2,
+								slidesToScroll: 2,
+							},
+						},
+						{
+							breakpoint: 768,
+							settings: {
+								slidesToShow: 3,
+								slidesToScroll: 3,
+							},
+						},
+					]}
+					dots={true}
+					showSides={true}
+					sidesOpacity={.5}
+					sideSize={.1}
+					slidesToScroll={4}
+					slidesToShow={4}
+					scrollOnDevice={true}
+				>
+					<div>
+						<img
+							alt=''
+							src={yetti}
+						/>
 					</div>
-					<div classname="timers"></div>
-					<div classname="slidePrev"><span></span></div>
-					<div classname="slideNext"><span></span></div>
-				</div>
-
+					<div>
+						<img
+							alt=''
+							src={boxTee}
+						/>
+					</div>
+					<div>
+						<img
+							alt=''
+							src={hoodie}
+						/>
+					</div>
+					<div>
+						<img
+							alt=''
+							src={hat}
+						/>
+					</div>
+					<div>
+						<img
+							alt=''
+							src={tee}
+						/>
+					</div>
+					<div>
+						<img
+							alt=''
+							src={grimHoodie}
+						/>
+					</div>
+				</InfiniteCarousel>
 			</div>
+
+
+
+
+
 			<div classname="main">
 				<div classname="content-bottom">
 					<div classname="container">
@@ -46,9 +130,9 @@ export default function HomeScreen() {
 							<div classname="col-md-7">
 								<h3>Fully Sent<br></br>About Us</h3>
 								<p class="m_1">
-								Fully Sent is a Drift Club & Social Club featured around extreme sports.
-								Founded by the stoke for anything on wheels, by a group of Florida natives, we wanted to create and share something new for the scene.
-								By supporting FullySent you are enabling us to follow our passions and create content for you!
+									Fully Sent is a Drift Club & Social Club featured around extreme sports.
+									Founded by the stoke for anything on wheels, by a group of Florida natives, we wanted to create and share something new for the scene.
+									By supporting FullySent you are enabling us to follow our passions and create content for you!
 								</p>
 							</div>
 						</div>
@@ -98,7 +182,7 @@ export default function HomeScreen() {
 						</div>
 					</div>
 				</div>
-			</div>	
+			</div>
 		</div>
 	)
 }
