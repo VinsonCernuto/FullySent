4bbe2c83f73c5f3a78d439f3e338ca8fd46ba0be
LyricDuda <Lyricsduda@gmail.com>
2020-11-22 18:24:03 -0500

    Sign in no reg

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,9 +5,16 @@ import {
     productDetailsReducer,
     productListReducer,
   } from './reducers/productReducers';
+  import { userSigninReducer } from './reducers/userReducer';
 
+  // ./reducers/userReducers
 
 const initalState = {
+  userSignin: {
+    userInfo: localStorage.getItem('userInfo')
+      ? JSON.parse(localStorage.getItem('userInfo'))
+      : null,
+  },
   cart: {
     cartItems: localStorage.getItem('cartItems')
       ? JSON.parse(localStorage.getItem('cartItems'))
@@ -18,6 +25,7 @@ const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
     cart: cartReducer,
+    userSignin: userSigninReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, initalState, composeEnhancer(applyMiddleware(thunk)));
